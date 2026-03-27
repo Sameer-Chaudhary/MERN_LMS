0912f0ad39eec766d0af6016b7441addbbcd932c
Sameer-Chaudhary <thesameer727@gmail.com>
2026-03-27 18:32:32 +0530

    Update axiosInstance.js

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://mern-lms-backend-jqqa.onrender.com",
 });
 
 axiosInstance.interceptors.request.use(
